41366d967cfe7c04e6c5da873fa894790e19a076
YuKyung-Chung <kadie@naver.com>
2022-11-15 02:51:00 +0900

    commit

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        //MemberService memberService = new MemberServiceImpl(); 직접 의존관계 설정하던 것에서 appConfig가 결정하도록 변경(의존성 주입)
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,30 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        System.out.println("before each test");
+    }
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "홍길동", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
